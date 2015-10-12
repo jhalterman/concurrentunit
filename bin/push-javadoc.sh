60f4bccf33645657d189eb3355cd4f0cb8229cba
Jonathan Halterman <jhalterman@gmail.com>
2015-10-11 17:23:03 -0700

    Setup travis to auto-build javadoc

--- bin/push-javadoc.sh
+++ bin/push-javadoc.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # run from top level dir
+
+PROJECT=concurrentunit
+
+mvn javadoc:javadoc
+
 rm -rf target/docs
-git clone git@github.com:jhalterman/concurrentunit.git target/docs -b gh-pages
-mvn -Pjavadoc javadoc:javadoc
+git clone git@github.com:jhalterman/$PROJECT.git target/docs -b gh-pages
 cd target/docs
-git add -A
+git rm -rf javadoc
+mkdir -p javadoc
+mv ../../target/site/apidocs/* javadoc
+git add -A -f javadoc
 git commit -m "Updated JavaDocs"
-git push origin gh-pages
+git push -fq origin gh-pages
